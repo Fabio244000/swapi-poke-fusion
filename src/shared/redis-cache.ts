589d8b1b9469df68b5fe917e32212f75e55bb3b7
Fabio <fabio.nm.19@gmail.com>
2025-05-28 19:12:15 -0500

    added redis cache class

---
+++ src/shared/redis-cache.ts
@@ -0,0 +1,18 @@
+import { createClient } from 'redis';
+
+const client = createClient({
+  url: `redis://:${process.env.REDIS_PORT}@${process.env.REDIS_HOST}`
+});
+
+export async function get<T>(key: string): Promise<T | null> {
+  await client.connect();
+  const v = await client.get(key);
+  await client.quit();
+  return v ? JSON.parse(v) as T : null;
+}
+
+export async function set<T>(key: string, value: T, ttl = Number(process.env.REDIS_TTL)): Promise<void> {
+  await client.connect();
+  await client.set(key, JSON.stringify(value), { EX: ttl });
+  await client.quit();
+}
